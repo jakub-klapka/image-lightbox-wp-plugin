66bc699a57e4b21bc853d7019e79fd5ea1e031a5
Jakub Klapka <klapka@lumiart.cz>
2014-04-30 20:01:03 +0200

    Refactor code based on WP standards

--- core/FrontendScriptLoading.frontend.php
+++ core/FrontendScriptLoading.frontend.php
@@ -8,8 +8,7 @@ class FrontendScriptLoading {
 
 	private $script_loaded = false;
 
-	public function __construct()
-	{
+	public function __construct() {
 		add_action( 'wp', array( $this, 'check_for_fancybox' ) );
 		add_action( 'init', array( $this, 'check_for_filters_init' ) );
 		add_action( 'get_header', array( $this, 'check_for_filters_get_header' ) );
@@ -18,9 +17,8 @@ class FrontendScriptLoading {
 	/**
 	 * Those checks are postponed, so theme can modify it's behavior via filters
 	 */
-	public function check_for_filters_init()
-	{
-		if( apply_filters( 'lumi-image-fancybox/enable_gallery', true ) ) {
+	public function check_for_filters_init() {
+		if ( apply_filters( 'lumi-image-fancybox/enable_gallery', true ) ) {
 			add_action( 'wp', array( $this, 'check_for_gallery' ) );
 		}
 	}
@@ -28,25 +26,22 @@ class FrontendScriptLoading {
 	/**
 	 * Postpone this filter as much as possible, so theme can run some own checks first
 	 */
-	public function check_for_filters_get_header()
-	{
-		if( apply_filters( 'lumi-image-fancybox/allways_load_frontend_script', false ) ) {
+	public function check_for_filters_get_header() {
+		if ( apply_filters( 'lumi-image-fancybox/allways_load_frontend_script', false ) ) {
 			$this->load_frontend_script();
 		};
 	}
 
 
-
-	private function load_frontend_script()
-	{
-		if( $this->script_loaded === false ) {
+	private function load_frontend_script() {
+		if ( $this->script_loaded === false ) {
 			$jquery_handle = apply_filters( 'lumi-image-fancybox/jquery_library', 'jquery' );
-			wp_enqueue_script( 'lumi-fancybox', plugins_url( 'js/lumi-fancybox.js', dirname( __FILE__ ) ), array($jquery_handle), LUMI_IFB_CSS_JS_VER, true );
+			wp_enqueue_script( 'lumi-fancybox', plugins_url( 'js/lumi-fancybox.js', dirname( __FILE__ ) ), array( $jquery_handle ), LUMI_IFB_CSS_JS_VER, true );
 
-			wp_enqueue_style('lumi-fancybox', plugins_url( 'css/lumi-fancybox.css', dirname( __FILE__ ) ), array(), LUMI_IFB_CSS_JS_VER );
+			wp_enqueue_style( 'lumi-fancybox', plugins_url( 'css/lumi-fancybox.css', dirname( __FILE__ ) ), array(), LUMI_IFB_CSS_JS_VER );
 
 			$fancybox_additional_config = apply_filters( 'lumi-image-fancybox/fancybox_js_config', false );
-			if( is_array( $fancybox_additional_config ) ) {
+			if ( is_array( $fancybox_additional_config ) ) {
 				wp_localize_script( 'lumi-fancybox', 'LumiFancyboxConfig', $fancybox_additional_config );
 			}
 
@@ -54,18 +49,16 @@ class FrontendScriptLoading {
 		}
 	}
 
-	public function check_for_fancybox($wp)
-	{
+	public function check_for_fancybox( $wp ) {
 		global $post;
-		if( strpos( $post->post_content, 'lumi-fancybox' ) !== false ) {
+		if ( strpos( $post->post_content, 'lumi-fancybox' ) !== false ) {
 			$this->load_frontend_script();
 		}
 	}
 
-	public function check_for_gallery()
-	{
+	public function check_for_gallery() {
 		global $post;
-		if( strpos( $post->post_content, '[gallery' ) !== false ) {
+		if ( strpos( $post->post_content, '[gallery' ) !== false ) {
 			$this->load_frontend_script();
 			lumi_ifb_ondemand( 'GalleryShortcodeModification' );
 		}
--- core/GalleryShortcodeModification.ondemand.php
+++ core/GalleryShortcodeModification.ondemand.php
@@ -6,12 +6,14 @@ namespace Lumi_IFB\OnDemand;
 
 class GalleryShortcodeModification {
 
-	public function __construct()
-	{
-		add_filter( 'post_gallery', array( $this, 'gallery_output' ), 8, 2 ); //higher priority, so theme can override that
+	public function __construct() {
+		add_filter( 'post_gallery', array(
+				$this,
+				'gallery_output'
+			), 8, 2 ); //higher priority, so theme can override that
 	}
 
-	public function gallery_output( $output, $attr ){
+	public function gallery_output( $output, $attr ) {
 
 		/**
 		 * Based on wp-includes/media.php
@@ -19,60 +21,84 @@ class GalleryShortcodeModification {
 		$post = get_post();
 
 		static $instance = 0;
-		$instance++;
+		$instance ++;
 
 		// We're trusting author input, so let's at least make sure it looks like a valid orderby statement
 		if ( isset( $attr['orderby'] ) ) {
 			$attr['orderby'] = sanitize_sql_orderby( $attr['orderby'] );
-			if ( !$attr['orderby'] )
+			if ( ! $attr['orderby'] ) {
 				unset( $attr['orderby'] );
+			}
 		}
 
 		$html5 = current_theme_supports( 'html5', 'gallery' );
-		extract(shortcode_atts(array(
+		extract( shortcode_atts( array(
 			'order'      => 'ASC',
 			'orderby'    => 'menu_order ID',
 			'id'         => $post ? $post->ID : 0,
-			'itemtag'    => $html5 ? 'figure'     : 'dl',
-			'icontag'    => $html5 ? 'div'        : 'dt',
+			'itemtag'    => $html5 ? 'figure' : 'dl',
+			'icontag'    => $html5 ? 'div' : 'dt',
 			'captiontag' => $html5 ? 'figcaption' : 'dd',
 			'columns'    => 3,
 			'size'       => 'thumbnail',
 			'include'    => '',
 			'exclude'    => '',
 			'link'       => ''
-		), $attr, 'gallery'));
+		), $attr, 'gallery' ) );
 
-		$id = intval($id);
+		$id = intval( $id );
 		if ( 'RAND' == $order )
 			$orderby = 'none';
 
-		if ( !empty($include) ) {
-			$_attachments = get_posts( array('include' => $include, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+		if ( ! empty( $include ) ) {
+			$_attachments = get_posts( array(
+					'include'        => $include,
+					'post_status'    => 'inherit',
+					'post_type'      => 'attachment',
+					'post_mime_type' => 'image',
+					'order'          => $order,
+					'orderby'        => $orderby
+				) );
 
 			$attachments = array();
 			foreach ( $_attachments as $key => $val ) {
-				$attachments[$val->ID] = $_attachments[$key];
+				$attachments[ $val->ID ] = $_attachments[ $key ];
 			}
-		} elseif ( !empty($exclude) ) {
-			$attachments = get_children( array('post_parent' => $id, 'exclude' => $exclude, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+		} elseif ( ! empty( $exclude ) ) {
+			$attachments = get_children( array(
+					'post_parent'    => $id,
+					'exclude'        => $exclude,
+					'post_status'    => 'inherit',
+					'post_type'      => 'attachment',
+					'post_mime_type' => 'image',
+					'order'          => $order,
+					'orderby'        => $orderby
+				) );
 		} else {
-			$attachments = get_children( array('post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+			$attachments = get_children( array(
+					'post_parent'    => $id,
+					'post_status'    => 'inherit',
+					'post_type'      => 'attachment',
+					'post_mime_type' => 'image',
+					'order'          => $order,
+					'orderby'        => $orderby
+				) );
 		}
 
-		if ( empty($attachments) )
+		if ( empty( $attachments ) )
 			return '';
 
 		if ( is_feed() ) {
 			$output = "\n";
 			foreach ( $attachments as $att_id => $attachment )
-				$output .= wp_get_attachment_link($att_id, $size, true) . "\n";
+				$output .= wp_get_attachment_link( $att_id, $size, true ) . "\n";
+
 			return $output;
 		}
 
-		$itemtag = tag_escape($itemtag);
-		$captiontag = tag_escape($captiontag);
-		$icontag = tag_escape($icontag);
+		$itemtag    = tag_escape( $itemtag );
+		$captiontag = tag_escape( $captiontag );
+		$icontag    = tag_escape( $icontag );
 		$valid_tags = wp_kses_allowed_html( 'post' );
 		if ( ! isset( $valid_tags[ $itemtag ] ) )
 			$itemtag = 'dl';
@@ -81,9 +107,9 @@ class GalleryShortcodeModification {
 		if ( ! isset( $valid_tags[ $icontag ] ) )
 			$icontag = 'dt';
 
-		$columns = intval($columns);
-		$itemwidth = $columns > 0 ? floor(100/$columns) : 100;
-		$float = is_rtl() ? 'right' : 'left';
+		$columns   = intval( $columns );
+		$itemwidth = $columns > 0 ? floor( 100 / $columns ) : 100;
+		$float     = is_rtl() ? 'right' : 'left';
 
 		$selector = "gallery-{$instance}";
 
@@ -120,7 +146,7 @@ class GalleryShortcodeModification {
 		</style>\n\t\t";
 		}
 
-		$size_class = sanitize_html_class( $size );
+		$size_class  = sanitize_html_class( $size );
 		$gallery_div = "<div id='$selector' class='gallery galleryid-{$id} gallery-columns-{$columns} gallery-size-{$size_class}'>";
 
 		/**
@@ -129,7 +155,7 @@ class GalleryShortcodeModification {
 		 * @since 2.5.0
 		 *
 		 * @param string $gallery_style Default gallery shortcode CSS styles.
-		 * @param string $gallery_div   Opening HTML div container for the gallery shortcode output.
+		 * @param string $gallery_div Opening HTML div container for the gallery shortcode output.
 		 */
 		$output = apply_filters( 'gallery_style', $gallery_style . $gallery_div );
 
@@ -141,9 +167,9 @@ class GalleryShortcodeModification {
 			//Add fancybox stuff
 			$fancybox_markup = sprintf( ' data-lumi-fancybox="true" rel="lumi-fancybox-gallery-%s"',
 				$instance );
-			$image_output = preg_replace( '/((?:.+)?<a\shref=[\'"].+?[\'"])((?:.+)?)/', '$1' . $fancybox_markup . '$2', $image_output );
+			$image_output    = preg_replace( '/((?:.+)?<a\shref=[\'"].+?[\'"])((?:.+)?)/', '$1' . $fancybox_markup . '$2', $image_output );
 
-			$image_meta  = wp_get_attachment_metadata( $id );
+			$image_meta = wp_get_attachment_metadata( $id );
 
 			$orientation = '';
 			if ( isset( $image_meta['height'], $image_meta['width'] ) )
@@ -154,10 +180,10 @@ class GalleryShortcodeModification {
 			<{$icontag} class='gallery-icon {$orientation}'>
 				$image_output
 			</{$icontag}>";
-			if ( $captiontag && trim($attachment->post_excerpt) ) {
+			if ( $captiontag && trim( $attachment->post_excerpt ) ) {
 				$output .= "
 				<{$captiontag} class='wp-caption-text gallery-caption'>
-				" . wptexturize($attachment->post_excerpt) . "
+				" . wptexturize( $attachment->post_excerpt ) . "
 				</{$captiontag}>";
 			}
 			$output .= "</{$itemtag}>";
